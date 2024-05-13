e1c0b94fe445e3d05fec8ab3284c71b7f0d5e254
Aditya <code.adi8103@gmail.com>
2024-05-13 23:32:59 +0530

    Auth pushed2

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,58 +1,57 @@
-const User=require("../models/userModel")
-const bcrypt=require("bcrypt")
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const SECRET = process.env.SECRET;
 
+const SignUp = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
 
-module.exports.SignUp= async(req,res)=>{
-   
-   try{
-   const {username,email,password}=req.body;
-
-   const usernameCheck=await User.findOne({username});
-   if(usernameCheck)
-   {
-      return res.json({msg:"Username already used",status:false});
-   }
-   const emailCheck=await User.findOne({email});
-   if(emailCheck)
-   {
-      return res.json({msg:"Email already used",status:false});
-   }
-   const hashedPassword=await bcrypt.hash(password,10);
-   const user=await User.create({
+    const usernameCheck = await User.findOne({ username });
+    if (usernameCheck) {
+      return res.json({ msg: "Username already used", status: false });
+    }
+    const emailCheck = await User.findOne({ email });
+    if (emailCheck) {
+      return res.json({ msg: "Email already used", status: false });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
       email,
       username,
-      password:hashedPassword,
-   });
-   delete user.password;
-   return res.json({status:true,user});
-   }
-   catch(err)
-   {
-      console.log(err.message);
-   }
+      password: hashedPassword,
+    });
+    const token = jwt.sign({ id: user.id }, SECRET);
+    return res.json({ status: true, user, token: token });
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
-module.exports.SignIn= async(req,res)=>{
-   
-    try{
-    const {username,password}=req.body;
- 
-    const isUserValid=await User.findOne({username});
-    if(!isUserValid)
-    {
-       return res.json({msg:"Invalid username or password",status:false});
+const SignIn = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const isUserValid = await User.findOne({ username });
+    if (!isUserValid) {
+      return res.json({ msg: "Invalid username or password", status: false });
     }
-    
-    const isPasswordvalid=await bcrypt.compare(password,isUserValid.password);
-    if(!isPasswordvalid)
-    {
-       return res.json({msg:"Invalid username or Password",status:false})
+
+    const isPasswordvalid = await bcrypt.compare(
+      password,
+      isUserValid.password
+    );
+    if (!isPasswordvalid) {
+      return res.json({ msg: "Invalid username or Password", status: false });
     }
-    delete isUserValid.password;
-    return res.json({status:true,isUserValid});
-    }
-    catch(err)
-    {
-       console.log(err.message);
-    }
-}
+    const token = jwt.sign({ id: isUserValid.id }, SECRET);
+    return res.json({ status: true, isUserValid, token: token });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+module.exports = {
+  SignUp,
+  SignIn,
+};
--- server/index.js
+++ server/index.js
@@ -1,32 +1,18 @@
-const express= require('express');
-const cors= require('cors');
-const mongoose=require('mongoose');
-const userRoutes=require("./routes/userRoutes.js");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes.js");
+const connectToMongo = require("./Database/db.js");
 
 require("dotenv").config();
 const app = express();
+connectToMongo(process.env.URL);
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", userRoutes);
 
-
-app.use("/api/auth",userRoutes);
-
-mongoose.connect(process.env.MONGODB_URL)
-.then(()=>
-{
-    console.log("server is connected to database");
-})
-.catch((err)=>{
-    console.log(err.message);
-})
-
-
-app.listen(process.env.PORT,()=>
-{
-    console.log(`server is running on port ${process.env.PORT}`);
-})
-
-
-
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+});
