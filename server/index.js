733a43fd173b678dc391cb41a7cc065aa710255e
Aditya <code.adi8103@gmail.com>
2024-05-14 10:33:37 +0530

    custom error handlers Pushed

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes.js");
 const connectToMongo = require("./Database/db.js");
+const errorHandler = require("./middleware/errorHandler.js");
 
 require("dotenv").config();
 const app = express();
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running on port ${process.env.PORT}`);
